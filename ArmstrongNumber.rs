c7def31ab8cbf9990b2636847ef69c2f72fa39b8
Bat-Erdene847-3RD3N3 <51613411+Bat-Erdene847-3RD3N3@users.noreply.github.com>
2019-12-05 19:21:37 +0800

    Update ArmstrongNumber.rs

--- ArmstrongNumber.rs
+++ ArmstrongNumber.rs
@@ -1,1 +1,23 @@
+fn is_armstrong_number(num: i32) -> bool {
+    let len = ((num + 1) as f32).log10().ceil() as i32;
+    let strigified = num.to_string();
+    let armstrong: i32 = strigified.chars()
+        .map(|digit| digit.to_digit(10).unwrap())
+        .map(|digit| digit.pow(len))
+        .sum();
+    armstrong == num
+}
+fn get_input() -> String {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).expect("Failed");
+    buffer
+}
 
+fn main() {
+    let num = get_input().trim().parse::<i32>().unwrap();
+    if is_armstrong_number(num) {
+        println!("armstrong number");
+    } else {
+        println!("not armstrong number");
+    }
+}
